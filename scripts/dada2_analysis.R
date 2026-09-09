006b05f913a9881ef05f6052663bb6f57adff7b4
MOISECHRIST <moisechristianjunior464@gmail.com>
2026-09-10 00:56:17 +0200

    Taking in account when the learning error fail and handle the error with exceptions

--- scripts/dada2_analysis.R
+++ scripts/dada2_analysis.R
@@ -28,29 +28,58 @@ dir.create(path.figures, recursive = TRUE, showWarnings = FALSE)
 dir.create(path.rds, recursive = TRUE, showWarnings = FALSE)
 
 #Learning errors 
-errorEstFunc.list <- c(loessErrfun, PacBioErrfun, makeBinnedQualErrfun,
-                       loessErrfun_mod0, loessErrfun_mod1, loessErrfun_mod2, 
-                       loessErrfun_mod3, loessErrfun_mod4)
+errorEstFunc.list <- c("loessErrfun"=loessErrfun, 
+                      "PacBioErrfun"=PacBioErrfun, 
+                      "makeBinnedQualErrfun"=makeBinnedQualErrfun,
+                      "loessErrfun_mod0"=loessErrfun_mod0, 
+                      "loessErrfun_mod1"=loessErrfun_mod1, 
+                      "loessErrfun_mod2"=loessErrfun_mod2, 
+                      "loessErrfun_mod3"=loessErrfun_mod3, 
+                      "loessErrfun_mod4"=loessErrfun_mod4)
 err <- list()
 dd_res <- list()
 track <- list()
-for(func in errorEstFunc.list){
-  func.name <- deparse(substitute(func))
-  if(func.name == "makeBinnedQualErrfun"){
-    err[[func.name]] <- learnErrors(path.filts, 
-                                    errorEstimationFunction=func(c(3, 10, 17, 22, 27, 35, 40)), 
-                                    multithread=TRUE)
-  } else {
-    err[[func.name]] <- learnErrors(path.filts, errorEstimationFunction=func, 
-                                    multithread=TRUE)
+
+for(func_name in names(errorEstFunc.list)){
+
+  func <- errorEstFunc.list[[func_name]]
+
+  message(paste0("Using the error function : ",func_name))
+  err_obj <- tryCatch({
+    if (func_name == "makeBinnedQualErrfun") {
+      learnErrors(
+        path.filts,
+        errorEstimationFunction = func(c(3, 10, 17, 22, 27, 35, 40)),
+        multithread = TRUE
+      )
+    } else {
+      learnErrors(
+        path.filts,
+        errorEstimationFunction = func,
+        multithread = TRUE
+      )
+    }
+  }, error = function(e) {
+    warning(paste0("Failed to learn errors for ", func_name, ": ", e$message))
+    return(NULL)
+  })
+  
+  if (is.null(err_obj) || is.null(err_obj$err_out)) {
+    message(paste0("Skipping dada() for ", func_name, " due to error estimation failure."))
+    next
   }
-  pdf(file.path(path.figures, paste0("learn_error_plot_",func.name,".pdf")))
-  plotErrors(err[[func.name]])
+  
+  err[[func_name]] <- err_obj
+
+  pdf(file.path(path.figures, paste0("learn_error_plot_",func_name,".pdf")))
+  print(plotErrors(err[[func_name]]))
   dev.off()
-  saveRDS(err[[func.name]], file.path(path.rds, paste0("learn_error_data_",func.name,".rds")))
-  dd_res[[func.name]] <- dada(path.filts, err=err[[func.name]], multithread=TRUE)
-  track[[func.name]] <- sapply(dd_res[[func.name]], function(x) sum(x$denoised))
-  saveRDS(dd_res[[func.name]], file.path(path.rds, paste0("dada_results_",func.name,".rds")))
+
+  saveRDS(err[[func_name]], file.path(path.rds, paste0("learn_error_data_",func_name,".rds")))
+  dd_res[[func_name]] <- dada(path.filts, err=err[[func_name]], multithread=TRUE)
+
+  track[[func_name]] <- sapply(dd_res[[func_name]], function(x) sum(x$denoised))
+  saveRDS(dd_res[[func_name]], file.path(path.rds, paste0("dada_results_",func_name,".rds")))
 }
 
 track <- as.data.frame(track)
@@ -65,9 +94,5 @@ ggplot(track_long, aes(x = reorder(method, denoised, FUN = median), y = denoised
   labs(x = "Error model", y = "Reads denoised")
 
 ggsave(file.path(path.figures, "track_denoised_boxplot.pdf"), width = 8, height = 6)
-
-
-
-
 
 
