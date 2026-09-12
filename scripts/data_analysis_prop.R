63b4668baba0e356bf1f69437c4d26c8ceaf3f07
MOISECHRIST <moisechristianjunior464@gmail.com>
2026-09-12 18:37:14 +0200

    Moving data_analysis.R to data_analysis_prop.R for analysis and visualization of dataset from sampling of fastq by proportion

--- scripts/data_analysis_prop.R
+++ scripts/data_analysis_prop.R
@@ -77,7 +77,8 @@ plotDistanceBoxplot.prop <- function(distances.long){
 }
 
 #Input data
-path.alldataset <- file.path(list.files("results", full.names = T), "RDS")
+args <- commandArgs(trailingOnly=TRUE)
+path.alldataset <- file.path(list.files(args[1], full.names = T), "RDS")
 paths_learn_errors_data <- list.files(path.alldataset, pattern = "learn_error", full.names = T) 
 paths_dada2_results_data <- list.files(path.alldataset, pattern = "dada_results", full.names = T) 
 ref.db <- file.path("refSeq/SILVA-v138.2-16s/silva_nr99_v138.2_toSpecies_trainset.fa.gz")
