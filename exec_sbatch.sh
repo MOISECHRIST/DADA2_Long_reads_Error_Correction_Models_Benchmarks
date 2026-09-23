7e00aacfdfbe7524326840e4e69fd901b0ce3730
MOISECHRIST <moisechristianjunior464@gmail.com>
2026-09-23 12:46:48 +0200

    Adding a new alternative to run nbases with seed in exec_sbatch.sh script

--- exec_sbatch.sh
+++ exec_sbatch.sh
@@ -25,11 +25,20 @@ elif [ $step -eq 2 ]; then
             sbatch --array=0-$((DATASETCOUNT-1)) main.sh data/"${prop_seed}" "${func_name}"
         done 
     done 
-else 
+elif [ $step -eq 3 ]; then
     #LearnError + Denoising of the dataset in the nbases approach for each function
     for func_name in "${func_list[@]}"; do
         for nbases in $(echo "1e+04 1e+05 1e+06 1e+07"); do 
             sbatch --array=0-$((RAWDATASETCOUNT-1)) main.sh data/raw "${func_name}" "${nbases}" 
         done 
     done 
+else 
+    #LearnError + Denoising of the dataset in the nbases approach for each function + randomized sampling + multiple seed
+    for func_name in "${func_list[@]}"; do
+        for nbases in $(echo "1e+04 1e+05 1e+06 1e+07"); do 
+            for seed in $(echo "100 101 102 103"); do
+                sbatch --array=0-$((RAWDATASETCOUNT-1)) main.sh data/raw "${func_name}" "${nbases}" "${seed}"
+            done 
+        done 
+    done
 fi
